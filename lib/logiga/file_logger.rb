4b97b566c9b3a178eb611762c41fe21138b011ec
Heryandi <heryandi@gmail.com>
2014-06-18 14:49:42 +0800

    Change default shift_age to 0

--- lib/logiga/file_logger.rb
+++ lib/logiga/file_logger.rb
@@ -3,7 +3,7 @@ require 'logger'
 # Log to text file. Create the folder as well.
 module Logiga
   class FileLogger < ::Logger
-    def initialize(filename, shift_age = 7, shift_size = 1048576)
+    def initialize(filename, shift_age = 0, shift_size = 1048576)
       filepath_str = File.expand_path(filename)
       filedir_str = File.dirname(filepath_str)
       Pathname.new(filedir_str).mkpath()
